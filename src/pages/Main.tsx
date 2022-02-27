0bbb12f201295703d3e82b771bc985ce20d758cb
남현우 <hwnim5324@naver.com>
2022-02-27 22:22:18 +0900

    Setup basic development environment.

---
+++ src/pages/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Main.scss';
+
+class Main extends React.Component {
+  render() {
+    return(
+      <div>
+        <span className='txt'>hello world</span>
+      </div>
+    );
+  }
+}
+
+
+export default Main;
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Main.scss';
+
+class SignUp extends React.Component {
+    render() {
+        return(
+            <div>
+                <span className='txt'>Sign Up</span>
+            </div>
+        );
+    }
+}
+
+
+export default SignUp;
