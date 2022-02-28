5953a72198bb16218ce6175ddb5d1ee511a77765
남현우 <hwnim5324@naver.com>
2022-03-01 00:49:19 +0900

    Organize header contents.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import '../styles/Layout.scss';
 
 const Layout = ( props : { children : React.ReactNode } ) => {
     return(
         <div>
-            <header>
-                <Menu />
+            <header className='header'>
+                <LeftTabNavigator />
             </header>
-            <div className='container'>
+            <main className='container'>
                 { props.children }
-            </div>
+            </main>
             <footer>
 
             </footer>
@@ -17,15 +18,38 @@ const Layout = ( props : { children : React.ReactNode } ) => {
     );
 }
 
-const Menu = () => {
+const LeftTabNavigator = () => {
     return(
-        <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/SignIn'>SignIn</Link></li>
-        </ul>
+        <div className='tab_navigator'>
+            <SignIn />
+            <Menu />
+        </div>
     );
 }
 
+const SignIn = () => {
+    return(
+        <div className='profile'>
+            <Link to='/SignIn' className='profile_signin'>
+                <span>로그인</span>
+            </Link>
+        </div>
+    );
+}
+
+const Menu = () => {
+    return(
+        <nav className='menu'>
+            <ul>
+                <li className='menu_item'>
+                    <Link to='/' className='menu_item_link'>
+                        <span>Home</span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
 
 export default Layout;
 
